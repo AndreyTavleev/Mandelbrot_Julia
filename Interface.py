Add SSAA. Fix colouring regime switcher. Now the image updates without re-calculation.

Interface.py
@@ -66,6 +66,7 @@ class DialogSave(BaseDialog):
         ui.lineEdit_L.setText('1000')
         ui.lineEdit_H.setText('1000')
         ui.lineEdit_DPI.setText(str(int(parent.fig.dpi)))
+        ui.lineEdit_SS.setText('1')
         ui.lineEdit_DPI.setEnabled(False)
 
         ui.label_Size.setText('Image size: \n1000 \U000000D7 1000 pix.')
@@ -116,22 +117,26 @@ class ImageExporter:
 
     def save_to_file(self, filename):
         self.save_image_dialog.ui.pushButton_Save.setEnabled(False)
-        length = int(self.save_image_dialog.ui.lineEdit_L.text())
-        height = int(self.save_image_dialog.ui.lineEdit_H.text())
+        length = abs(int(self.save_image_dialog.ui.lineEdit_L.text()))
+        height = abs(int(self.save_image_dialog.ui.lineEdit_H.text()))
+        ss_factor = abs(int(float(self.save_image_dialog.ui.lineEdit_SS.text())))
+        if ss_factor == 0:
+            ss_factor = 1
         img_width, img_height = length, height
         if self.save_image_dialog.ui.checkBox_withAxes.isChecked():
-            dpi = int(self.save_image_dialog.ui.lineEdit_DPI.text())
+            dpi = abs(int(self.save_image_dialog.ui.lineEdit_DPI.text()))
             img_width *= dpi
             img_height *= dpi
         xmin, xmax = self.ax.get_xlim()
         ymin, ymax = self.ax.get_ylim()
         print(xmin, xmax, ymin, ymax, self.x_c, self.y_c, self.horizon)
         data = fractal_set(xmin, xmax, ymin, ymax, x_c=self.x_c, y_c=self.y_c,
-                           height=img_height, length=img_width,
+                           height=img_height * ss_factor, length=img_width * ss_factor,
                            n=self.n, horizon=self.horizon, power=self.power,
                            mode=self.mode)[2].T
         if self.regime == 'sin':
             data = (np.sin(data * self.freq + self.offset)) ** 2
+        data = data.reshape((img_height, ss_factor, img_width, ss_factor)).mean(axis=(1, 3))
         if self.save_image_dialog.ui.checkBox_withAxes.isChecked():
             _, ax = plt.subplots(figsize=(length, height), dpi=dpi)
             if not self.shading:
@@ -158,11 +163,11 @@ class ImageExporter:
         self.save_image_dialog.ui.pushButton_Save.setEnabled(True)
 
     def edit_size_label(self):
-        ll = int(self.save_image_dialog.ui.lineEdit_L.text())
-        h = int(self.save_image_dialog.ui.lineEdit_H.text())
+        ll = abs(int(self.save_image_dialog.ui.lineEdit_L.text()))
+        h = abs(int(self.save_image_dialog.ui.lineEdit_H.text()))
         if self.save_image_dialog.ui.checkBox_withAxes.isChecked():
             self.save_image_dialog.ui.lineEdit_DPI.setEnabled(True)
-            dpi = int(self.save_image_dialog.ui.lineEdit_DPI.text())
+            dpi = abs(int(self.save_image_dialog.ui.lineEdit_DPI.text()))
             self.save_image_dialog.ui.label_Size.setText(f'Image size: \n{ll * dpi} \U000000D7 {h * dpi} pix.')
         else:
             self.save_image_dialog.ui.lineEdit_DPI.setEnabled(False)
@@ -320,7 +325,7 @@ class MJSet(MyWindowMandelbrotJulia, FractalControls, CoordinateManager, JuliaPa
         self.invalid_slider = False
         self.cache = None
 
-        # Initialize dialogues and toolbar
+        # Initialise dialogues and toolbar
         self.main_layout = None
         self.set_limits_dialog, self.save_image_dialog = None, None
         self.set_c_dialog, self.shading_dialog = None, None
@@ -329,7 +334,7 @@ class MJSet(MyWindowMandelbrotJulia, FractalControls, CoordinateManager, JuliaPa
         self.toolbar = None
 
     def initialize_ui_components(self):
-        """Initializes UI components and configures their default values."""
+        """Initialises UI components and configures their default values."""
         self.ui.groupbox_C.setVisible(False)
         self.ui.lineEdit_freq.setVisible(False)
         self.ui.label_freq.setVisible(False)

colour_controls.py
@@ -257,23 +257,34 @@ class ColourManager:
         regime = self.ui.comboBox_regime.currentText()
         if self.regime == regime:
             return
-        self.regime = self.ui.comboBox_regime.currentText()
-        im = self.ax.images[0]
+        self.regime = regime
         if regime == 'standard':
-            im.set_array(self.cache)
-            self.cache = None
             self.ui.lineEdit_freq.setVisible(False)
             self.ui.label_freq.setVisible(False)
             self.ui.lineEdit_offset.setVisible(False)
             self.ui.label_offset.setVisible(False)
         elif regime == 'sin':
-            self.cache = im.get_array()
-            data = (np.sin(self.cache * self.freq + self.offset)) ** 2
-            im.set_array(data)
             self.ui.lineEdit_freq.setVisible(True)
             self.ui.label_freq.setVisible(True)
             self.ui.lineEdit_offset.setVisible(True)
             self.ui.label_offset.setVisible(True)
+        else:
+            raise ValueError('Regime must be standard or sin.')
+
+        if self.shading and not self.no_ax_update:
+            self.ax_update()
+            return
+
+        im = self.ax.images[0]
+        if regime == 'standard':
+            im.set_array(self.cache)
+            self.cache = None
+        elif regime == 'sin':
+            self.cache = im.get_array()
+            data = (np.sin(self.cache * self.freq + self.offset)) ** 2
+            im.set_array(data)
+        else:
+            raise ValueError('Regime must be standard or sin.')
         im.set(clim=(im.get_array().min(), im.get_array().max()))
         self.fig.canvas.draw_idle()
 
@@ -285,7 +296,7 @@ class ColourManager:
             im.set_array(data)
             im.set(clim=(im.get_array().min(), im.get_array().max()))
             self.fig.canvas.draw_idle()
-        else:
+        elif not self.no_ax_update:
             self.ax_update()
 
     def set_offset(self):
@@ -296,5 +307,5 @@ class ColourManager:
             im.set_array(data)
             im.set(clim=(im.get_array().min(), im.get_array().max()))
             self.fig.canvas.draw_idle()
-        else:
+        elif not self.no_ax_update:
             self.ax_update()

zoom_animation.py
@@ -33,7 +33,7 @@ def make_colourmap(colours_data):
 
 def make_frame(i, scale, xmin_1, xmax_1, ymin_1, ymax_1, xmin_2, xmax_2, ymin_2, ymax_2,
                mode, x_c, y_c, power, n_regime, n_i, n_f, horizon, length, height, colourmap,
-               c_regime, freq, offset, shading, azdeg, altdeg, vert_exag, path, frames):
+               c_regime, freq, offset, shading, azdeg, altdeg, vert_exag, supersampling, path, frames):
     """Generate a single frame for the zoom animation."""
     xmin_3 = (1 - scale) * xmin_1 + scale * xmin_2
     ymin_3 = (1 - scale) * ymin_1 + scale * ymin_2
@@ -52,18 +52,19 @@ def make_frame(i, scale, xmin_1, xmax_1, ymin_1, ymax_1, xmin_2, xmax_2, ymin_2,
 
     print(f'Frame {i + 1} / {frames}')
 
-    data = fractal_set(xmin_3, xmax_3, ymin_3, ymax_3, horizon=horizon, length=length, height=height,
-                       n=n, x_c=x_c, y_c=y_c, power=power, mode=mode)[2].T
+    data = fractal_set(xmin_3, xmax_3, ymin_3, ymax_3, horizon=horizon, length=length*supersampling,
+                       height=height*supersampling, n=n, x_c=x_c, y_c=y_c, power=power, mode=mode)[2].T
     if c_regime == 'standard':
         pass
     elif c_regime == 'sin':
         data = (np.sin(data * freq + offset)) ** 2
+    data = data.reshape((height, supersampling, length, supersampling)).mean(axis=(1, 3))
     if shading:
         light = colors.LightSource(azdeg=azdeg, altdeg=altdeg)
         data = light.shade(data, cmap=plt.get_cmap(colourmap), vert_exag=vert_exag,
                            blend_mode='hsv')
     plt.imsave(path + f'image_{i:d}.png', data,
-               cmap=colourmap if not shading else None)  # , origin='lower')
+               cmap=colourmap if not shading else None, origin='lower')
 
 
 def validate_aspect_ratio(delta_x_1, delta_y_1, delta_x_2, delta_y_2, length, height):
@@ -138,7 +139,7 @@ def generate_video(path, name, fps=30):
 def main(metadata, x_centre_1, y_centre_1, delta_x_1, delta_y_1,
          x_centre_2, y_centre_2, delta_x_2, delta_y_2, x_c, y_c,
          mode, power, n_regime, n_i, n_f, horizon, frames, length, height, colourmap,
-         c_regime, freq, offset, shading, azdeg, altdeg, vert_exag, threads, path):
+         c_regime, freq, offset, shading, azdeg, altdeg, vert_exag, threads, supersampling, path):
     """Main function to generate the zoom animation."""
     if not isinstance(colourmap, str):
         colourmap = make_colourmap(colourmap)
@@ -186,13 +187,16 @@ def main(metadata, x_centre_1, y_centre_1, delta_x_1, delta_y_1,
     ymin_2 = y_centre_2 - delta_y_2 / 2
     ymax_2 = y_centre_2 + delta_y_2 / 2
 
+    if supersampling == 0:
+        supersampling = 1
+
     time0 = dt.now()
     scales = 1.0 - np.logspace(0, -50, frames, base=2, dtype=np.float64)
     pool = mp.Pool(threads)
     result = [pool.apply_async(make_frame, args=(i, scale, xmin_1, xmax_1, ymin_1, ymax_1, xmin_2, xmax_2,
                                                  ymin_2, ymax_2, mode, x_c, y_c, power, n_regime, n_i, n_f, horizon,
                                                  length, height, colourmap, c_regime, freq, offset, shading, azdeg,
-                                                 altdeg, vert_exag, path, frames))
+                                                 altdeg, vert_exag, abs(supersampling), path, frames))
               for i, scale in enumerate(scales)]
 
     im_arr = [res.get() for res in result]
@@ -283,6 +287,8 @@ if __name__ == '__main__':
                         help='Vertical exaggeration factor for shading relief (default: 1.0).')
     parser.add_argument('-t', '--threads', type=int, default=mp.cpu_count() - 2,
                         help='Number of threads to use for frame creation (default: number of CPU cores - 2).')
+    parser.add_argument('-ss', '--supersampling', type=int, default=1,
+                        help='Supersampling (SSAA) factor for the image rendering (default: 1).')
     args = parser.parse_args()
     if args.horizon is None:
         if args.mode == 'mandelbrot':